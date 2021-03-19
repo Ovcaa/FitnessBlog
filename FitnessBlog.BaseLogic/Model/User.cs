5879cfa280af397277c8175977251446bc244f35
Ovcaa <78650740+Ovcaa@users.noreply.github.com>
2021-03-19 22:42:26 +0500

    Добавьте файлы проекта.

---
+++ FitnessBlog.BaseLogic/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessBlog.BaseLogic.Model
+{
+    public class User
+    {
+        public string Name { get; } 
+        public Gender gender { get; }
+        public DateTime birthDate { get; }
+    }
+}
